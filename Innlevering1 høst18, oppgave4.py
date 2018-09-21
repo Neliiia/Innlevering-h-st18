aea08207bdd115cb5dbd7de7dd74e65b50d33ad6
Neliiia <43165367+Neliiia@users.noreply.github.com>
2018-09-21 10:57:43 +0200

    Add files via upload

---
+++ Innlevering1 høst18, oppgave4.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 20 11:55:48 2018
+
+@author: Åsne Holo
+"""
+
+n=float(input("Hvilket tall vil du regne faktuleten til?")) #deklarerer en variabalen n som er tallet vi vil regne fakulteten til
+a=1 #deklarerer variabelen a og gir den verdien 1 enn så lenge
+
+if n%1==0: #sjekker om n er et heltall, altså om resten er null når den deles på 1
+    for i in range (1, round(n)+1): #løkke fra i=0 til og med i=n
+        a*=i #tilegner a en ny verdi a*i for hver i fra 0 til n
+    print(round(n), "!=", a) #skriver ut a
+else:
+    print("Tallet du skrev inn er ikke et heltall, prøv igjen") #feilmelding hvis n ikke er et heltall
