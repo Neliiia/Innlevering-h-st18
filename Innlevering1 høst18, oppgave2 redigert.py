249744bd9be7f184e2bc9999beb60cc9043eb3e3
Neliiia <43165367+Neliiia@users.noreply.github.com>
2018-09-21 11:00:46 +0200

    Add files via upload

--- Innlevering1 høst18, oppgave2 redigert.py
+++ Innlevering1 høst18, oppgave2 redigert.py
@@ -1,39 +1,16 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Sep 17 14:59:36 2018
+Created on Fri Sep 21 10:58:57 2018
 
 @author: Åsne Holo
 """
 
-from math import *
+n=float(input("Hvilket tall vil du regne faktuleten til?")) #deklarerer en variabalen n som er tallet vi vil regne fakulteten til
+a=1 #deklarerer variabelen a og gir den verdien 1 enn så lenge
 
-def Ef(): #lager en funksjon som spør etter E
-    global E #definerer E til å være en global variabel
-    E=float(input("Hva er den kinetiske energien i J? ")) #tilegner E en float-verdi    
-def mf(): #funksjon som spør etter m
-    global m #m: en global variabel
-    m=float(input("Hva er massen i kg? ")) #tilegner m en float-verdi
-def vf(): #funksjon som spør etter v
-    global v #v: global variabel
-    v=float(input("Hva er farten i m/s? ")) #tilegner v en float-verdi
-
-a=input("Vil du regne ut E, m eller v? ") #spør hva man vil regne ut
-
-#avhengig av svaret på a, kjører programmet ett av tre mulige alternativer med funksjoner
-if a=="E":
-    mf()
-    vf()
-    E=m/2*v**2 #regner ut energien
-    print("Energien er", E, "J") #printer energien
-elif a=="m":
-    Ef()
-    vf()
-    m=2*E/v**2 #regner ut massen
-    print("Massen er", m, "kg") #printer massen
-elif a=="v":
-    Ef()
-    mf()
-    v=sqrt(2*E/m) #regner ut farten
-    print("Farten er", v, "m/s") #printer farten
+if n%1==0 and n>0: #sjekker om n er et heltall, altså om resten er null når den deles på 1
+    for i in range (1, round(n)+1): #løkke fra i=0 til og med i=n
+        a*=i #tilegner a en ny verdi a*i for hver i fra 0 til n
+    print(round(n), "!=", a) #skriver ut a
 else:
-    print("Du må velge enten E, m eller v. Prøv igjen")
+    print("Tallet du skrev inn er ikke et positivt heltall, prøv igjen") #feilmelding hvis n ikke er et positivt heltall
